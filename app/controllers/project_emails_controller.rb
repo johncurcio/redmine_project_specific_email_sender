ae394c0a68fa80d8adf822b6c3ba73002d436783
Alex Shulgin <ash@commandprompt.com>
2011-07-04 16:37:05 +0300

    Removed hand-made remote form submission from the view. Moved 'Restore Defaults' button to a contextual link (titled 'Reset'.) Improved request handling in ProjectEmailsController.

--- app/controllers/project_emails_controller.rb
+++ app/controllers/project_emails_controller.rb
@@ -4,42 +4,34 @@ class ProjectEmailsController < ApplicationController
   before_filter :load_project, :authorize
   
   def update
-    @project.email = params[:email]
-    respond_to do |format|
-      format.html{
-        if @project.project_email.save
-          flash[:notice] = "Email successfully updated"
-        else
-          flash[:notice] = "Email format invalid"
-        end
-        redirect_to :controller => "projects", :action => "settings"
-      }
-      format.js{
-        if @project.project_email.save
-          render :text => "success"
-        else
-          render :text => "failure"
-        end
-      }
+    url = project_settings_tab_url
+
+    email = (params[:project] || {})[:email]
+    @project.email = email
+    if @project.project_email.save
+      flash[:notice] = l(:notice_successful_update)
+    else
+      flash[:error] = @project.project_email.errors.full_messages.join
+      url[:email] = email
     end
+    redirect_to url
   end
   
   def restore_default
     @project.project_email.destroy if @project.project_email
     @project.reload
-    respond_to do |format|
-      format.html{
-        flash[:notice] = "Email restored to system default"
-        redirect_to :controller => "projects", :action => "settings"
-      }
-      format.js{
-        render :text => @project.email
-      }
-    end
+
+    redirect_to project_settings_tab_url, :notice => l(:notice_email_restored)
   end
   
   private
+  
   def load_project
     @project = Project.find(params[:id])
+  end
+
+  def project_settings_tab_url
+    { :controller => 'projects', :action => 'settings',
+      :id => @project, :tab => 'outbound_email' }
   end
 end
