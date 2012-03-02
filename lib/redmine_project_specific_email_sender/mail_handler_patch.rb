4934428006ffb1ea1809ac0f0278874d26471087
Alex Shulgin <ash@commandprompt.com>
2012-03-02 16:11:35 +0200

    Add MailHandlerPatch: check for project's email in To header specifically (ignore Cc.)

---
+++ lib/redmine_project_specific_email_sender/mail_handler_patch.rb
@@ -0,0 +1,21 @@
+module RedmineProjectSpecificEmailSender
+  module MailHandlerPatch
+    def self.included(base)
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        alias_method_chain :receive_issue, :project_specific_email
+      end
+    end
+    
+    module InstanceMethods
+      def receive_issue_with_project_specific_email
+        if @email.to_addrs.map(&:spec).include? target_project.email
+          receive_issue_without_project_specific_email
+        else
+          logger.info "Not creating new ticket as project email is not in the To: header" if logger
+          true # so the controller returns successful status
+        end
+      end
+    end
+  end
+end
