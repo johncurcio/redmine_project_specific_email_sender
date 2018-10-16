0539ed118ebc7c0418eade1d32c1a2135cc8bc58
John Curcio <joao8177@gmail.com>
2018-10-16 11:02:42 -0300

    fix: specif email role permission

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_project_specific_email_sender do
   author 'Adam Walters'
   description "This is a plugin for Redmine which allows each project to have it's own sender email address for project related, outbound emails"
   version '1.0.0'
-  
-  permission :edit_project_email, :project_emails => [:update, :destroy]
+
+  permission :edit_project_email, {:projects => :settings, :project_emails => [:update, :destroy]}
 end
 
 prepare_block = Proc.new do
